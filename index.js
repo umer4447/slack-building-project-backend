443f6442a65c745ebdef03bf9095dbd133b6fa71
Muhammad Umer <umer44447@gmail.com>
2022-06-30 11:49:39 -0700

    Feature/Enhancement-Authentication

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 
+const authRoutes = require("./routes/auth.js")
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -10,3 +12,10 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.get('/', (req,res) => {
+    res.send('Hello, World!')
+})
+app.use('/auth', authRoutes );
+
+app.listen(PORT, () =>console.log(`Server is running on Port ${PORT}`));
